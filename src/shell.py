3567729eb226e6bb483f8c319500e35ba44e4151
miberl <m.berlingieri02@gmail.com>
2022-10-11 15:56:34 +0100

    Moved apps to separate module

--- src/shell.py
+++ src/shell.py
@@ -1,98 +1,12 @@
 import re
 import sys
 import os
-from os import listdir
 from collections import deque
 from glob import glob
+import apps.apps as apps
 
-
-def pwd(args, out) -> None:
-    out.append(os.getcwd())
-
-
-def cd(args, out) -> None:
-    if len(args) == 0 or len(args) > 1:
-        raise ValueError("wrong number of command line arguments")
-    os.chdir(args[0])
-
-
-def echo(args, out) -> None:
-    out.append(" ".join(args) + "\n")
-
-
-def ls(args, out) -> None:
-    if len(args) == 0:
-        ls_dir = os.getcwd()
-    elif len(args) > 1:
-        raise ValueError("wrong number of command line arguments")
-    else:
-        ls_dir = args[0]
-    for f in listdir(ls_dir):
-        if not f.startswith("."):
-            out.append(f + "\n")
-
-
-def cat(args, out) -> None:
-    for a in args:
-        with open(a) as f:
-            out.append(f.read())
-
-
-def head(args, out) -> None:
-    if len(args) != 1 and len(args) != 3:
-        raise ValueError("wrong number of command line arguments")
-    if len(args) == 1:
-        num_lines = 10
-        file = args[0]
-    if len(args) == 3:
-        if args[0] != "-n":
-            raise ValueError("wrong flags")
-        else:
-            num_lines = int(args[1])
-            file = args[2]
-    with open(file) as f:
-        lines = f.readlines()
-        for i in range(0, min(len(lines), num_lines)):
-            out.append(lines[i])
-
-
-def tail(args, out) -> None:
-    if len(args) != 1 and len(args) != 3:
-        raise ValueError("wrong number of command line arguments")
-    if len(args) == 1:
-        num_lines = 10
-        file = args[0]
-    if len(args) == 3:
-        if args[0] != "-n":
-            raise ValueError("wrong flags")
-        else:
-            num_lines = int(args[1])
-            file = args[2]
-    with open(file) as f:
-        lines = f.readlines()
-        display_length = min(len(lines), num_lines)
-        for i in range(0, display_length):
-            out.append(lines[len(lines) - display_length + i])
-
-
-def grep(args, out) -> None:
-    if len(args) < 2:
-        raise ValueError("wrong number of command line arguments")
-    pattern = args[0]
-    files = args[1:]
-    for file in files:
-        with open(file) as f:
-            lines = f.readlines()
-            for line in lines:
-                if re.match(pattern, line):
-                    if len(files) > 1:
-                        out.append(f"{file}:{line}")
-                    else:
-                        out.append(line)
-
-
-apps = {'pwd': pwd, 'cd': cd, 'echo': echo, 'ls': ls,
-        'cat': cat, 'head': head, 'tail': tail, 'grep': grep}
+appList = {'pwd': apps.pwd, 'cd': apps.cd, 'echo': apps.echo, 'ls': apps.ls,
+           'cat': apps.cat, 'head': apps.head, 'tail': apps.tail, 'grep': apps.grep}
 
 
 def eval(cmdline, out):
@@ -115,8 +29,8 @@ def eval(cmdline, out):
         app = tokens[0]
         args = tokens[1:]
 
-        if app in apps:
-            apps[app](args, out)
+        if app in appList:
+            appList[app](args, out)
         else:
             raise ValueError(f"unsupported application {app}")
 
