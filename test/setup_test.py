fdf2ca2e162e20643ac08f7ff8067d3583b13811
miberl <m.berlingieri02@gmail.com>
2022-10-14 19:55:05 +0100

    Update test image var

--- test/setup_test.py
+++ test/setup_test.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 class TestSetup(unittest.TestCase):
-    SHELL_IMAGE = "comp0010-system-test"
+    SHELL_IMAGE = "comp0010-test"
     TEST_VOLUME = "comp0010-test-volume"
     TEST_IMAGE = "comp0010-test-image"
     TEST_DIR = "/test"
