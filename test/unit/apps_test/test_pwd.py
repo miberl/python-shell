ccb568d1cb16445863b16980357f1f4030f8148d
miberl <m.berlingieri02@gmail.com>
2022-10-14 21:56:31 +0100

    cd pwd integration tests

--- test/unit/apps_test/test_pwd.py
+++ test/unit/apps_test/test_pwd.py
@@ -3,4 +3,4 @@ from setup_test import TestSetup
 
 class TestPwd(TestSetup):
     def test_matches_current_directory(self):
-        self.run_test("pwd", ['/test'])
+        self.run_test("pwd", [self.TEST_DIR])
