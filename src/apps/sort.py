3ba328d64000f9f0396429a1de5b1c7e7487323d
miberl <m.berlingieri02@gmail.com>
2022-10-29 21:38:10 +0100

    added reverse sort

--- src/apps/sort.py
+++ src/apps/sort.py
@@ -4,7 +4,17 @@ class Sort(Application):
     
     def run(self, args, out)-> None:
         lines = []
-        for a in args:
-            lines.extend(self.read_lines(a))
-        lines.sort()
+        rev = self.is_reversed(args)
+        
+        files = [a for a in args if a[0] != "-"]
+
+        for file in files:
+            lines.extend(self.read_lines(file))
+        
+        lines.sort(reverse=rev)
         out.extend(lines)
+
+    def is_reversed(self, args)-> bool:
+        if "-r" in args:
+            return True
+        return False
