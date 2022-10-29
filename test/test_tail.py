c7d44b255523eaae7c5fcae9237c0308f4b34b8a
miberl <m.berlingieri02@gmail.com>
2022-10-29 22:07:41 +0100

    refactored application patching in unit tests

--- test/test_tail.py
+++ test/test_tail.py
@@ -11,28 +11,26 @@ class TestTail(TestSetup):
         self.out = []
         self.app = Tail()
 
+    def run_test(self, args, expected_output):
+        super().run_test(args, expected_output, "application.Application.read_lines", TestSetup.mock_read_lines)
+
     # HAPPY PATHS
 
     def test_tail(self):
-        self.run_test(["test.txt"], ["Test\n"],
-                      "apps.tail.Tail.read_lines", TestSetup.mock_read_lines)
+        self.run_test(["test.txt"], ["Test\n"])
 
     def test_tail_2(self):
-        self.run_test(["dir1/file1.txt"],  ["AAA\n", "BBB\n", "AAA\n"],
-                      "apps.tail.Tail.read_lines", TestSetup.mock_read_lines)
+        self.run_test(["dir1/file1.txt"],  ["AAA\n", "BBB\n", "AAA\n"])
 
     # Expects last 10 lines of file as return
     def test_tail_longfile(self):
         expected_output = [f"{str(i)}\n" for i in range(11, 21)]
-        self.run_test(["dir1/longfile.txt"], expected_output,
-                      "apps.tail.Tail.read_lines", TestSetup.mock_read_lines)
+        self.run_test(["dir1/longfile.txt"], expected_output)
 
     def test_tail_specific_line_limit(self):
         expected_output = [f"{str(i)}\n" for i in range(16, 21)]
-        self.run_test(["-n", "5", "dir1/longfile.txt"], expected_output,
-                      "apps.tail.Tail.read_lines", TestSetup.mock_read_lines)
+        self.run_test(["-n", "5", "dir1/longfile.txt"], expected_output)
 
     def test_tail_specific_line_limit_2(self):
         expected_output = [f"{str(i)}\n" for i in range(1, 21)]
-        self.run_test(["-n", "100", "dir1/longfile.txt"], expected_output,
-                      "apps.tail.Tail.read_lines", TestSetup.mock_read_lines)
+        self.run_test(["-n", "100", "dir1/longfile.txt"], expected_output)
--- test/test_uniq.py
+++ test/test_uniq.py
@@ -8,26 +8,23 @@ class TestUniq(TestSetup):
         self.out = []
         self.app = Uniq()
 
+    def run_test(self, args, expected_output):
+        super().run_test(args, expected_output, "application.Application.read_lines", TestSetup.mock_read_lines)
+
     def test_uniq(self):
         self.run_test(
             ["test.txt"],
-            ["Test\n"],
-            "apps.uniq.Uniq.read_lines",
-            TestSetup.mock_read_lines,
+            ["Test\n"]
         )
 
     def test_uniq_2(self):
         self.run_test(
             ["dir1/file1.txt"],
-            ["AAA\n", "BBB\n", "AAA\n"],
-            "apps.uniq.Uniq.read_lines",
-            TestSetup.mock_read_lines,
+            ["AAA\n", "BBB\n", "AAA\n"]
         )
 
     def test_uniq_3(self):
         self.run_test(
             ["repeatedFile.txt"],
-            ["AAA\n", "BBB\n", "AAA\n", "\n", "BBB\n"],
-            "apps.uniq.Uniq.read_lines",
-            TestSetup.mock_read_lines,
+            ["AAA\n", "BBB\n", "AAA\n", "\n", "BBB\n"]
         )
