467b5dfb5386caad4ca922a501568b4ef385abac
Filipp Kondrashov <zcabfko@ucl.ac.uk>
2022-11-20 14:06:54 +0000

    added support for piping and redirections

--- src/syntax_highlighting/syntax_highlighter.py
+++ src/syntax_highlighting/syntax_highlighter.py
@@ -2,23 +2,15 @@ from os import getcwd
 
 
 class shell_colours:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-
+    APP = "\u001b[34;1m"
     FLAG = "\033[96m"
-    APP = "\033[96m"
-    ARG = "\033[96m"
     PIPE = "\033[96m"
     REDIR_IN = "\033[96m"
     REDIR_OUT = "\033[96m"
-
+    DIR = "\033[96m"
+    FAIL = "\033[91m"
+    UNDERLINE = "\033[4m"
+    ENDC = "\033[0m"
 
     LINE_CLEAR = "\x1b[2K"
     LINE_UP = "\033[1A"
@@ -34,24 +26,20 @@ class syntax_highlighter:
         return code
 
     def highlight_code(self, code) -> str:
-        new_code = self.highlight_app(new_code)
-        new_code = self.highlight_flags(code)
-        return new_code
-
-    def highlight_flags(self, code) -> str:
         words = code.split()
         new_code = ""
+        previous_word = ""
         for word in words:
             if word.startswith("-"):
                 new_code += shell_colours.FLAG + word + shell_colours.ENDC + " "
+            elif word.startswith("|"):
+                new_code += shell_colours.PIPE + word + shell_colours.ENDC + " "
+            elif word.startswith("<"):
+                new_code += shell_colours.REDIR_IN + word + shell_colours.ENDC + " "
+            elif word.startswith(">"):
+                new_code += shell_colours.REDIR_OUT + word + shell_colours.ENDC + " "
             else:
-                new_code += word + " "
-        return new_code
+                new_code += shell_colours.ARG + word + shell_colours.ENDC + " "
+            previous_word = word
 
-    def highlight_app(self, code) -> str:
-        words = code.split()
-        new_code = ""
-        words[0] = shell_colours.APP + words[0] + shell_colours.ENDC
-        for word in words:
-            new_code += word + " "
         return new_code
